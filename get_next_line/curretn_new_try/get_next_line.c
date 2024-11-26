1715c98f5d5f15c83ed4af9d1d8f5b211933a7d2
Bender352 <svenbruck@hotmail.com>
2024-11-26 17:26:40 +0100

    Update get_next_line.c

--- get_next_line/curretn_new_try/get_next_line.c
+++ get_next_line/curretn_new_try/get_next_line.c
@@ -12,18 +12,18 @@
 
 #include "get_next_line.h"
 
-char	*get_joined_buffer(char *buffer, int fd)
+char	*get_joined_buffer(char *buffer, int fd, long buf_size)
 {
-	char	temp[BUFFER_SIZE + 1];
+	char	temp[buf_size + 1];
 	long	i;
 
 	i = -1;
-	while (++i <= BUFFER_SIZE)
+	while (++i <= buf_size)
 		temp[i] = '\0';
 	i = 1;
 	while (!ft_strchr(temp, '\n') && i)
 	{
-		i = read(fd, temp, BUFFER_SIZE);
+		i = read(fd, temp, buf_size);
 		if (i < 0 || (!i && !buffer))
 			return (NULL);
 		temp[i] = '\0';
@@ -43,10 +43,15 @@ char	*get_next_line(int fd)
 	char		*ret_line;
 	char		*temp;
 	int			i;
+	long		buf_size;
 
-	if (fd < 0 || BUFFER_SIZE < 1 || read(fd, 0 ,0))
+	if (BUFFER_SIZE > 100)
+		buf_size = 100;
+	else
+		buf_size = BUFFER_SIZE;
+	if (fd < 0 || buf_size < 1 || read(fd, 0, 0))
 		return (NULL);
-	buffer = get_joined_buffer(buffer, fd);
+	buffer = get_joined_buffer(buffer, fd, buf_size);
 	if (!buffer)
 		return (NULL);
 	i = 0;
