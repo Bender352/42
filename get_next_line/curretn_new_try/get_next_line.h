10fa5c1cbc39c6dd6e610d9bab51651bb5b947a2
Bender352 <svenbruck@hotmail.com>
2024-11-26 17:26:59 +0100

    Update get_next_line.h

--- get_next_line/curretn_new_try/get_next_line.h
+++ get_next_line/curretn_new_try/get_next_line.h
@@ -14,14 +14,16 @@
 # define GET_NEXT_LINE_H
 
 # include <fcntl.h>
-# include <unistd.h>
+# include <stdint.h>
 # include <stdlib.h>
+# include <unistd.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
 
 char	*get_next_line(int fd);
+char	*get_joined_buffer(char *buffer, int fd, long buf_size);
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
