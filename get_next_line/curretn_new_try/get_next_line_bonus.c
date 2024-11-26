1d46ca205d8fcd48bc4a0410cf568cfab62c3761
Bender352 <svenbruck@hotmail.com>
2024-11-26 17:27:29 +0100

    Update get_next_line_bonus.c

--- get_next_line/curretn_new_try/get_next_line_bonus.c
+++ get_next_line/curretn_new_try/get_next_line_bonus.c
@@ -1,29 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sbruck <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 17:55:47 by sbruck            #+#    #+#             */
-/*   Updated: 2024/11/10 17:55:49 by sbruck           ###   ########.fr       */
+/*   Updated: 2024/11/26 15:14:34 by sbruck           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-char	*get_joined_buffer(char *buffer, int fd)
+char	*get_joined_buffer(char *buffer, int fd, long buf_size)
 {
-	char	temp[BUFFER_SIZE + 1];
+	char	temp[buf_size + 1];
 	long	i;
 
 	i = -1;
-	while (++i <= BUFFER_SIZE)
+	while (++i <= buf_size)
 		temp[i] = '\0';
 	i = 1;
 	while (!ft_strchr(temp, '\n') && i)
 	{
-		i = read(fd, temp, BUFFER_SIZE);
+		i = read(fd, temp, buf_size);
 		if (i < 0 || (!i && !buffer))
 			return (NULL);
 		temp[i] = '\0';
@@ -43,17 +43,23 @@ char	*get_next_line(int fd)
 	char		*ret_line;
 	char		*temp;
 	int			i;
+	long		buf_size;
 
-	if (fd < 0 || BUFFER_SIZE < 1 || read(fd, 0 ,0) || fd > OPEN_MAX)
+	if (BUFFER_SIZE > 100)
+		buf_size = 100;
+	else
+		buf_size = BUFFER_SIZE;
+
+	if (fd < 0 || buf_size < 1)
 		return (NULL);
-	buffer[fd] = get_joined_buffer(buffer[fd], fd);
+	buffer[fd] = get_joined_buffer(buffer[fd], fd, buf_size);
 	if (!buffer[fd])
 		return (NULL);
 	i = 0;
 	while (buffer[fd][i] != '\n' && buffer[fd][i])
-		i++;
+		++i;
 	if (buffer[fd][i] == '\n')
-		i++;
+		++i;
 	ret_line = ft_substr(buffer[fd], 0, i);
 	temp = ft_substr(buffer[fd], i, ft_strlen(buffer[fd]) - i);
 	free(buffer[fd]);
