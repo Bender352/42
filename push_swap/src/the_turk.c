5d72a45db982eaa8ba59d1154ddd00b180b6aeb3
Bender <bender352@gmail.com>
2025-02-09 01:29:30 +0100

    upload

---
+++ push_swap/src/the_turk.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   the_turk.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbruck <sbruck@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 16:15:30 by sbruck            #+#    #+#             */
+/*   Updated: 2025/02/09 01:29:03 by sbruck           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int cal_cost_push(t_stack **stack)
+{
+    (void) stack;
+    return (0);
+}
+
+int cal_cost_rotate (t_stack **stack)
+{
+    (void) stack;
+    return (0);
+}
+
+t_stack *find_low_coast (t_stack **stack)
+{
+    (void)stack;
+    return NULL;
+}
+
+void ini_stack (t_mothership m)
+{
+    set_index(m->stack_a);
+    set_index(m->stack_b);
+    set_target(m);
+    set_cost(m);
+    cheapest_node(stack_a);
+}
+
+void set_index (t_stack **stack)
+{
+    int i;
+    int median;
+    t_stack *list;
+
+    if (!stack && !(*stack))
+        return;
+    median = count_nodes(stack) / 2;
+    list = (*stack);
+    i = 0;
+    while (list)
+    {
+        list->index = i;
+        if (i <= median)
+            list->above_median = true;
+        else
+            list->above_median = false;
+        list = list->next;
+        i++;
+    }
+}
+
+void sort_stack (t_mothership m)
+{
+    int len;
+
+    len = count_nodes(m->stack_a);
+    if (len > 3)
+
+
+}
